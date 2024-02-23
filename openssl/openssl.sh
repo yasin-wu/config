da7ae4f5bbaf7602d83e4dd3678b05f8c9e472d0
yasin <yasin_wu@qq.com>
2024-02-23 10:39:57 +0800

    perf: openssl.sh

--- openssl/openssl.sh
+++ openssl/openssl.sh
@@ -7,7 +7,7 @@ set -e
 dns="DNS:localhost"
 ip="IP:127.0.0.1"
 days=3650
-dir="./tmp/"
+dir="./tmp"
 subj="/C=CN/ST=ChengDu/L=ChengDu/O=Yasin/OU=Yasin/CN=yasin.com.cn"
 
 while getopts ":d:i:" opt
@@ -37,7 +37,7 @@ if [ ! -d tmp ]; then
   mkdir tmp
 fi
 
-cat > ${dir}my-openssl.cnf << EOF
+cat > ${dir}/my-openssl.cnf << EOF
 [ ca ]
 default_ca = CA_default
 [ CA_default ]
@@ -64,30 +64,30 @@ basicConstraints       = CA:true
 EOF
 
 ####### 自签名CA根证书
-openssl genrsa -out ${dir}ca.key 2048
-openssl req -x509 -new -nodes -key ${dir}ca.key -subj ${subj} -days ${days} -out ${dir}ca.crt
+openssl genrsa -out ${dir}/ca.key 2048
+openssl req -x509 -new -nodes -key ${dir}/ca.key -subj ${subj} -days ${days} -out ${dir}/ca.crt
 
 ####### server certificate
-openssl genrsa -out ${dir}server.key 2048
-openssl req -new -sha256 -key ${dir}server.key \
+openssl genrsa -out ${dir}/server.key 2048
+openssl req -new -sha256 -key ${dir}/server.key \
     -subj ${subj} \
     -reqexts SAN \
-    -config <(cat ${dir}my-openssl.cnf <(printf "\n[SAN]\nsubjectAltName=${dns},${ip}")) \
-    -out ${dir}server.csr
+    -config <(cat ${dir}/my-openssl.cnf <(printf "\n[SAN]\nsubjectAltName=${dns},${ip}")) \
+    -out ${dir}/server.csr
 openssl x509 -req -days ${days} \
-    -in ${dir}server.csr -CA ${dir}ca.crt -CAkey ${dir}ca.key -CAcreateserial \
+    -in ${dir}/server.csr -CA ${dir}/ca.crt -CAkey ${dir}/ca.key -CAcreateserial \
     -extfile <(printf "subjectAltName=${dns},${ip}") \
-    -out ${dir}server.crt
+    -out ${dir}/server.crt
 
 ####### client certificate
-openssl genrsa -out ${dir}client.key 2048
+openssl genrsa -out ${dir}/client.key 2048
 openssl req -new \
     -subj ${subj} \
-    -config ${dir}my-openssl.cnf \
-    -key ${dir}client.key -out ${dir}client.csr
+    -config ${dir}/my-openssl.cnf \
+    -key ${dir}/client.key -out ${dir}/client.csr
 openssl x509 -req -days ${days} -sha256 \
-    -in ${dir}client.csr -CA ${dir}ca.crt -CAkey ${dir}ca.key -CAcreateserial \
-    -out ${dir}client.crt
+    -in ${dir}/client.csr -CA ${dir}/ca.crt -CAkey ${dir}/ca.key -CAcreateserial \
+    -out ${dir}/client.crt
 
 ####### delete unused file
-rm -rf ${dir}ca.key ${dir}ca.srl ${dir}client.csr ${dir}server.csr ${dir}my-openssl.cnf
+rm -rf ${dir}/ca.key ${dir}/ca.srl ${dir}/client.csr ${dir}/server.csr ${dir}/my-openssl.cnf
